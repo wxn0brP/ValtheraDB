cc7126241797b030c43121db808a27fa396421b4
wxn0brP <toja.filiper@gmail.com>
2025-02-25 22:06:02 +0100

    fix: browser support

--- cdn/kyShim.js
+++ cdn/kyShim.js
@@ -1,12 +1,17 @@
 const kyShim = {
-    post: (url, options) => fetch(url, { 
-        method: "POST", 
-        body: JSON.stringify(options.json), 
-        headers: { 
-            ...options.headers, 
-            "Content-Type": "application/json" 
-        } 
-    }).then(res => res.json())
+    post: (url, options) => ({
+        json: async () => {
+            const res = await fetch(url, { 
+                method: "POST", 
+                body: JSON.stringify(options.json), 
+                headers: { 
+                    ...options.headers, 
+                    "Content-Type": "application/json" 
+                } 
+            });
+            return res.json();
+        }
+    })
 };
 
 export default kyShim;
