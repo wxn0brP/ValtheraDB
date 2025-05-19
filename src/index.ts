f7f5423054a2abe2355480d2185080930c13ceb4
wxn0brP <toja.filiper@gmail.com>
2025-05-19 15:55:14 +0200

    refactor: organize types

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import ValtheraMemory, { createMemoryValthera } from "./actions/memory";
 import { ValtheraAutoCreate } from "./helpers/autoCreate";
 import { RelationTypes } from "./types/relation";
 import { ValtheraCompatible } from "./types/valthera";
+import { ValtheraTypes } from "./types/export";
 
 export {
     Valthera,
@@ -29,16 +30,7 @@ export type {
     ValtheraCompatible,
     RelationTypes,
     GraphCompatible,
+    ValtheraTypes
 }
 
 export type Id = import("./types/Id").Id;
-export namespace ValtheraTypes {
-    export type Arg = import("./types/arg").Arg;
-    export type Search = import("./types/arg").Search;
-    export type Updater = import("./types/arg").Updater;
-    export type DbFindOpts = import("./types/options").DbFindOpts;
-    export type FindOpts = import("./types/options").FindOpts;
-    export type DbOpts = import("./types/options").DbOpts;
-    export type Data = import("./types/data").Data;
-    export type SearchOptions = import("./types/searchOpts").SearchOptions;
-}
