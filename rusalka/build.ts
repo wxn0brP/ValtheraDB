8b5a05d0db007946523aa10087af044a07ff6393
wxn0brP <toja.filiper@gmail.com>
2025-11-13 17:24:19 +0100

    ci: migrate to rusalka

---
+++ rusalka/build.ts
@@ -0,0 +1,8 @@
+import { readFileSync, writeFileSync } from "fs";
+export const publishToNpm = true;
+export function postBuildFn() {
+    const version = JSON.parse(readFileSync("./package.json", "utf-8")).version;
+    writeFileSync("./dist/version.js", `export const version = "${version}";`);
+}
+
+export const typeDocs = 0;
