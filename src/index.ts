3558b993fe811117fa1e83e88111a750de2cdfa7
wxn0brP <toja.filiper@gmail.com>
2026-07-10 18:23:45 +0200

    feat!: bump deps & integrate resolver

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export type { Id } from "@wxn0brp/db-core/types/Id";
+export { VDB } from "@wxn0brp/db-resolver";
 export { ValtheraAutoRemoteCreate, ValtheraCreate } from "./autoCreate";
 
 export * from "@wxn0brp/db-client";
