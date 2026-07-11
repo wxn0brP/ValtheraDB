f09377e817da4eb6e346bf0dc029ce1383f7a082
wxn0brP <toja.filiper@gmail.com>
2026-07-11 15:45:57 +0200

    refactor: ValtheraCreate

--- src/autoCreate.ts
+++ src/autoCreate.ts
@@ -1,8 +1,10 @@
 import { RemoteConfig } from "@wxn0brp/db-client/remote";
 import { ValtheraRemote } from "@wxn0brp/db-client/valthera";
 import { forgeTypedValthera } from "@wxn0brp/db-core";
+import { ValtheraClass } from "@wxn0brp/db-core/db/valthera";
 import { Data } from "@wxn0brp/db-core/types/data";
 import { ValtheraCompatible } from "@wxn0brp/db-core/types/valthera";
+import { createFileActions } from "@wxn0brp/db-storage-dir";
 import { DbDirOpts } from "@wxn0brp/db-storage-dir/types";
 import { Valthera } from "./valthera";
 
@@ -29,5 +31,9 @@ export function ValtheraAutoRemoteCreate(cfg: string | RemoteConfig): ValtheraCo
  * @returns A new instance of Valthera.
  */
 export function ValtheraCreate<T extends Record<string, Data> = {}>(folder: string, options: DbDirOpts = {}) {
-    return forgeTypedValthera<T>(new Valthera(folder, options));
+    return forgeTypedValthera<T>(
+        new ValtheraClass({
+            adapter: createFileActions(folder, options)
+        })
+    );
 }
