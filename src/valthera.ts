0b0660506373e07e51758badea815eaab9c69625
wxn0brP <viviennevaltheris@gmail.com>
2026-08-11 14:35:19 +0200

    refactor: Valthera class

--- src/valthera.ts
+++ src/valthera.ts
@@ -1,10 +1,15 @@
 import { ValtheraClass } from "@wxn0brp/db-core/db/valthera";
+import type { VDB } from "@wxn0brp/db-resolver/v";
 import { FileActions } from "@wxn0brp/db-storage-dir";
 import { vFileCpu } from "@wxn0brp/db-storage-dir/file/index";
 import { DbDirOpts } from "@wxn0brp/db-storage-dir/types";
+import type { ValtheraCreate } from ".";
 
 /**
- * @deprecated use ValtheraCreate / VDB
+ * Low-level API.
+ *
+ * Prefer {@link ValtheraCreate} or {@link VDB} for most use cases.
+ * This class remains supported for advanced and specialized use cases.
  */
 export class Valthera extends ValtheraClass {
 	constructor(folder: string, options: DbDirOpts = {}, fileCpu = vFileCpu) {
